9d233ae32e14b44f6f3ac7a1a846f3800d06967b
SAMKELO MSHENGU <mshengusamkelo337@gmail.com>
2024-05-13 17:55:26 +0200

    added add branch page

---
+++ BestBrightness/Pages/AddBranch/AddBranch.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BestBrightness.Pages.AddBranch
+{
+    public class AddBranchModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
