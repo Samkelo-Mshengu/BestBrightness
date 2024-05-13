4220435ca9344e5de0b3e8f5b1ef0b764f37e8b8
SAMKELO MSHENGU <mshengusamkelo337@gmail.com>
2024-05-13 15:14:43 +0200

    add activity model

---
+++ DataLogic/Activity/ActivityHistory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLogic.Activity
+{
+    public class ActivityHistory
+    {
+        public Guid ActivityID { get; set; }
+        public Guid? MemberID { get; set; }
+        public DateTime? ActivityDate { get; set; }
+        public string ActivityType { get; set; }
+        public string Details { get; set; }
+    }
+}
