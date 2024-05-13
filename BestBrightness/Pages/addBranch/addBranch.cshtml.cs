6b7b1cd281274d689260d8fedfcaac76e94dd1c7
mondli24 <mondlimqanya@gmail.com>
2024-05-13 17:59:07 +0200

    MY ADD PAGE

---
+++ BestBrightness/Pages/addBranch/addBranch.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BestBrightness.Pages.addBranch
+{
+    public class addBranchModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
