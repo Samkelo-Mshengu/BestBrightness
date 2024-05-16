5caf493e05e4fcb0dbbf2d318f5bf6a22a484420
mondli24 <mondlimqanya@gmail.com>
2024-05-16 16:29:59 +0200

    Page working

--- BestBrightness/Pages/addBranch/addBranch.cshtml.cs
+++ BestBrightness/Pages/addBranch/addBranch.cshtml.cs
@@ -1,12 +1,34 @@
+using BusinesssLogic.LogicInterface;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ViewLogic.Provinces;
 
-namespace BestBrightness.Pages.addBranch
+namespace BestBrightness.Pages.AddBranch
 {
-    public class addBranchModel : PageModel
+    public class AddBranchModel : PageModel
     {
-        public void OnGet()
+
+        [BindProperty]
+        public List<ProvinceView> GetAllProvince {  get; set; }
+
+        public readonly IProvinceLogic _provinceLogic;
+
+       
+        public string SelectedProvince { get; set; }
+
+        public SelectList AllProvinces { get; set; }
+
+        public AddBranchModel(IProvinceLogic provinceLogic)
         {
+            _provinceLogic = provinceLogic;
+
+            GetAllProvince=new List<ProvinceView>();
+        }
+        public async Task OnGet()
+        {
+            GetAllProvince=await _provinceLogic.GetAllProvinceAsync();
+            Page();
         }
     }
 }
